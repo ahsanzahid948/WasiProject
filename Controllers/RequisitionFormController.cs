98bfdb94446f682d958183372e27d99b32f1b6d0
azahid <azahid@practiceehr.com>
2022-06-03 11:40:14 +0500

    Add Requisition Forn Index and New

--- Controllers/RequisitionFormController.cs
+++ Controllers/RequisitionFormController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManagementSystem.DAL.UnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,31 @@ namespace InventoryManagementSystem.Controllers
 {
     public class RequisitionFormController : Controller
     {
+        InventoryManagementSystemEntities context = new InventoryManagementSystemEntities();
+        private UnitOfWork unitOfWork = new UnitOfWork();
         // GET: RequisitionForm
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+        public ActionResult GetList()
+        {
+            var list = unitOfWork.GrnRepository.GetAll();
+
+
+            return Json(list);
+        }
+        public ActionResult Create(GRN Obj)
+        {
+            var a = context.GRNs.Add(Obj);
+            int b = context.SaveChanges();
+            return Json(a);
+        }
+        public ActionResult Update(GRN Obj)
         {
             return View();
         }
