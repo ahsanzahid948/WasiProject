9fc706b0c49e2d4456940b409427b9f54c3a243e
ahsanzhid <ahsanzahid948@gmail.com>
2022-06-19 03:50:52 -0700

    Report Finilized

--- Controllers/IsssueVoucherReportController.cs
+++ Controllers/IsssueVoucherReportController.cs
@@ -32,21 +32,47 @@ namespace InventoryManagementSystem.Controllers
                 
                 if (a.Count()>0)
                 {
+                    int? demandtotal=0, issuetotal=0, remainingtotoal=0;
+
+                    foreach (var n in a) {
+                        demandtotal = demandtotal +int.Parse( n.QTYDemand);
+
+                        issuetotal = issuetotal + int.Parse(n.QTYIssued);
+                        remainingtotoal = remainingtotoal + n.QTYRemaining;
+                    }
+
+
+
                     ReportDocument rd = new ReportDocument();
                 rd.Load(Server.MapPath("~/IssueVoucherReport.rpt"));
-             
-                rd.SetDataSource(a.ToList());
-                    
-                Response.Buffer = false;
-                Response.ClearContent();
-                Response.ClearHeaders();
-               
+                    string printedby = Session["FirstName"].ToString();
+                    rd.SetDataSource(a);
+                    rd.SetParameterValue("CompanyName", "National Institute Of Electronic");
+                    rd.SetParameterValue("AddressPhone", "H-9, Islamabad, Islamabad Capital Territory");
+                    rd.SetParameterValue("PrintedBy", printedby);
+                    rd.SetParameterValue("QTYDemandTotal", demandtotal);
+                    rd.SetParameterValue("QTYIssuedTotal", issuetotal);
+                    rd.SetParameterValue("QTYRemainingTotal", remainingtotoal);
+                    rd.SetParameterValue("logo", "");
+                    rd.SetParameterValue("Name", Name);
+                    Response.Buffer = false;
+                    Response.ClearContent();
+                    Response.ClearHeaders();
 
-            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
-                stream.Seek(0, SeekOrigin.Begin);
-                return File(stream, "application/pdf", "IssueVoucher.pdf");
-                
+               
+                    rd.PrintOptions.PaperOrientation = CrystalDecisions.Shared.PaperOrientation.Landscape;
+                    rd.PrintOptions.ApplyPageMargins(new CrystalDecisions.Shared.PageMargins(5, 5, 5, 5));
+                    rd.PrintOptions.PaperSize = CrystalDecisions.Shared.PaperSize.PaperA5;
+
+                    Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    return File(stream, "application/pdf", "IssueVoucherReport.pdf");
+
                 }
+
+
+
                 else { return Content("No Record Found"); }
 
             }
